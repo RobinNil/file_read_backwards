2d389d548aa54f3443fe1d973b24cc7606391851
Robin Robin <robin81@gmail.com>
2016-12-29 21:20:24 -0500

    mark it production/stable. tested this on several thousand files with no issues

--- setup.py
+++ setup.py
@@ -35,7 +35,7 @@ setup(
     zip_safe=False,
     keywords='file_read_backwards',
     classifiers=[
-        'Development Status :: 2 - Pre-Alpha',
+        'Development Status :: 5 - Production/Stable',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: MIT License',
         'Natural Language :: English',
