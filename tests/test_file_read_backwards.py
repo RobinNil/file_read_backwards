44b74a84a05bbbde3e53eb26d7c0bf5f58be369b
Samuel Giffard <samuel.giffard@unleashed.be>
2017-09-01 19:55:49 +0000

    FileReadBackwards is now a Context Manager.

--- tests/test_file_read_backwards.py
+++ tests/test_file_read_backwards.py
@@ -21,6 +21,27 @@ except NameError:
     xrange = range
 
 
+def helper_write(t, s, encoding="utf-8"):
+    """A helper method to write out string s in specified encoding."""
+    t.write(s.encode(encoding))
+
+
+def helper_create_temp_file(generator=None, encoding='utf-8'):
+    if generator is None:
+        generator = ("line {}!\n".format(i) for i in xrange(42))
+    temp_file = tempfile.NamedTemporaryFile(delete=False)
+    for line in generator:
+        helper_write(temp_file, line, encoding)
+    temp_file.close()
+    print('Wrote file {}'.format(temp_file.name))
+    return temp_file
+
+
+def helper_destroy_temp_file(temp_file):
+    temp_file.close()
+    os.unlink(temp_file.name)
+
+
 class TestFileReadBackwards(unittest.TestCase):
 
     """Class that contains various test cases for actual FileReadBackwards usage."""
@@ -144,3 +165,116 @@ class TestFileReadBackwards(unittest.TestCase):
             pass
         with self.assertRaises(NotImplementedError):
             _ = FileReadBackwards(t.name, encoding="not-supported-encoding")  # noqa: F841
+
+    def test_reusing_iterator(self):
+
+        with tempfile.NamedTemporaryFile(delete=False) as temp_file:
+            for line in ("line {}!\n".format(i) for i in xrange(42)):
+                helper_write(temp_file, line)
+        f = FileReadBackwards(temp_file.name, encoding='utf-8')
+        lines_read = deque()
+        for l in f:
+            lines_read.appendleft(l)
+        lines_read2 = deque()
+        for l2 in f:
+            lines_read2.appendleft(l2)
+        self.assertEqual(
+            lines_read,
+            lines_read2,
+            msg="Test that verifies that it creates a new iterator everytime"
+        )
+        os.unlink(temp_file.name)
+
+
+class TestFileReadBackwardsAsContextManager(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls):
+        cls.temp_file = helper_create_temp_file()
+
+    @classmethod
+    def tearDownClass(cls):
+        helper_destroy_temp_file(cls.temp_file)
+
+    def test_behaves_as_classic(self):
+        with FileReadBackwards(self.temp_file.name) as f:
+            lines_read = deque()
+            for l in f:
+                lines_read.appendleft(l)
+        f2 = FileReadBackwards(self.temp_file.name)
+        lines_read2 = deque()
+        for l2 in f2:
+            lines_read2.appendleft(l2)
+        self.assertEqual(
+            lines_read,
+            lines_read2,
+            msg="The Context Manager way should behave exactly the same way as without using one."
+        )
+
+    def test_weakref(self):
+        with FileReadBackwards(self.temp_file.name) as f:
+            it = iter(f)
+            self.assertEqual(len(f._FileReadBackwards__fp_refs), 1,
+                             msg="There should be one ref as one iterator was generated!")
+            del it
+            fps = f._FileReadBackwards__fp_refs
+            self.assertEqual(len(fps), 1, msg="The ref should still be there even though the the iterator was deleted.")
+            fp = fps[0]
+            self.assertIsNone(fp(), msg="The ref should be weak and not keep the object from being collected.")
+
+
+class TestFileReadBackwardsCloseFunctionality(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls):
+        cls.temp_file = helper_create_temp_file()
+
+    @classmethod
+    def tearDownClass(cls):
+        helper_destroy_temp_file(cls.temp_file)
+
+    def test_close_on_iterator(self):
+        with FileReadBackwards(self.temp_file.name) as f:
+            it = iter(f)
+            for count, i in enumerate(it):
+                if count == 2:
+                    break
+            self.assertFalse(it.closed, msg="The fp should not be closed when not exhausted")
+            it.close()
+            self.assertTrue(it.closed, msg="Calling close() on the iterator should close it")
+
+    def test_close_on_iterator_exhausted(self):
+        with FileReadBackwards(self.temp_file.name) as f:
+            it = iter(f)
+            for _ in it:
+                pass
+            self.assertTrue(it.closed, msg="The fp should be closed automatically when the iterator is exhausted.")
+
+    def test_close_on_reader_exit(self):
+        with FileReadBackwards(self.temp_file.name) as f:
+            it1 = iter(f)
+            it2 = iter(f)
+        self.assertTrue(it1.closed and it2.closed,
+                        msg="All iterators created by a reader should have their fp closed when the reader gets closed.")
+
+    def test_close_on_reader_explicitly(self):
+        f = FileReadBackwards(self.temp_file.name)
+        it1 = iter(f)
+        it2 = iter(f)
+        self.assertFalse(it1.closed or it2.closed, msg="Iterators should not have their fp closed at this point.")
+        f.close()
+        self.assertTrue(it1.closed and it2.closed,
+                        msg="All iterators created by a reader should have their fp closed when the reader closes them.")
+
+    def test_close_on_reader_with_already_closed_iterator(self):
+        with FileReadBackwards(self.temp_file.name) as f:
+            it = iter(f)
+            it.close()
+        self.assertTrue(it.closed, msg="It should be okay to close (through the reader) an already closed iterator.")
+
+    def test_cannot_iterate_when_closed(self):
+        with FileReadBackwards(self.temp_file.name) as f:
+            it = iter(f)
+            it.close()
+            for _ in it:
+                self.fail(msg="An iterator should be exhausted when closed.")
+
+
