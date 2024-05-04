bb79ef409cbf67cbd981aaea0f3a4cb8b2e22951
Robin Robin <robin81@gmail.com>
2024-05-04 13:24:14 -0400

    Bump version: 3.0.0 → 3.1.0

--- file_read_backwards/__init__.py
+++ file_read_backwards/__init__.py
@@ -4,4 +4,4 @@ from .file_read_backwards import FileReadBackwards  # noqa: F401
 
 __author__ = """Robin Robin"""
 __email__ = 'robinsquare42@gmail.com'
-__version__ = '3.0.0'
+__version__ = '3.1.0'
