dd4aca934781a44caa435858605a806b739e51d6
Robin Robin <robin81@gmail.com>
2025-04-26 08:12:03 -0400

    chore: remove support for eol'ed python
    Remove python 3.7 and 3.8 from support.
    They are EOL'ed on 2023-06-27 and 2024-10-07 respectively.

--- setup.py
+++ setup.py
@@ -39,8 +39,6 @@ setup(
         'Intended Audience :: Developers',
         'Natural Language :: English',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.9',
         'Programming Language :: Python :: 3.10',
         'Programming Language :: Python :: 3.11',
